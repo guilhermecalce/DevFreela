e7976ee912a323a5b04815e50bf4f8b305bfd609
Guilherme Calce da Silva <guilherme.calce@hotmail.com.br>
2024-07-31 17:23:43 -0300

    Commit DevFreela API

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,92 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly OpeningTimeOption _option;
+        public ProjectsController(IOptions<OpeningTimeOption> option, ExampleClass exampleClass)
+        {
+            exampleClass.Name = "Update at ProjectsController";
+            _option = option.Value;
+        }
+        // api/projects?query=net core
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            //Buscar todos os projetos
+            return Ok();
+        }
+        // api/projects?/599
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            //Buscar Projeto
+
+            // return NotFound();
+            return Ok();
+
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProjectModel)
+        {
+            if (createProjectModel.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            //Cadastrar Projeto
+            return CreatedAtAction(nameof(GetById), new { id = createProjectModel.Id }, createProjectModel);
+
+        }
+
+        // api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProjectModel)
+        {
+            if (updateProjectModel.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            //Atualiza o projeto
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            //Buscar projeto, se não existir retorna NotFound
+
+            //Remover
+            return NoContent(); 
+        }
+
+        // api/projects/1/comments POST
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, [FromBody] CreateCommentModel createCommentModel)
+        {
+            return NoContent();
+        }
+
+        // api/projects/1/start
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        // api/projects/1/finish
+
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
+    }
+}
